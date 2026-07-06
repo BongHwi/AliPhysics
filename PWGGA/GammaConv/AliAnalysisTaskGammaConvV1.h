65a3e3f9ad47599099711a09e083b580f111fde0
Stephan Friedrich Stiefelmaier <stephan.friedrich.stiefelmaier@cern.ch>
2026-07-06 11:41:55 +0200

    Increment class version from 78 to 79

--- PWGGA/GammaConv/AliAnalysisTaskGammaConvV1.h
+++ PWGGA/GammaConv/AliAnalysisTaskGammaConvV1.h
@@ -561,7 +561,7 @@ class AliAnalysisTaskGammaConvV1 : public AliAnalysisTaskSE {
 
     AliAnalysisTaskGammaConvV1(const AliAnalysisTaskGammaConvV1&); // Prevent copy-construction
     AliAnalysisTaskGammaConvV1 &operator=(const AliAnalysisTaskGammaConvV1&); // Prevent assignment
-    ClassDef(AliAnalysisTaskGammaConvV1, 78);
+    ClassDef(AliAnalysisTaskGammaConvV1, 79);
 };
 
 #endif
